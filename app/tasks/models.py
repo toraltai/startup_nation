adfc2cd89554abfc87f469437df5c32060aae39a
toraltai <flangkg@gmail.com>
2024-09-09 11:28:22 +0600

    tasks added

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,5 +1,4 @@
 from tortoise import Tortoise, fields
-from dataclasses import dataclass
 from tortoise.models import Model
 from tortoise.contrib.pydantic import pydantic_model_creator
 
@@ -7,22 +6,13 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 class Module(Model):
     id: int
     title = fields.CharField(25)
-    # level = fields.ForeignKeyField("models.Level", related_name="modules", on_delete=fields.CASCADE, null=True)
 
+    # Связь "Один ко многим" с уровнем
+    levels = fields.ReverseRelation["Level"]
 
 Tortoise.init_models(["app.tasks.models"], "models")
 GetModule = pydantic_model_creator(Module, name="Module")
 CreateModule = pydantic_model_creator(Module, name="ModuleIn", exclude_readonly=True)
-
-
-class Task(Model):
-    id = fields.IntField(pk=True)
-    title = fields.CharField(50)
-
-
-Tortoise.init_models(["app.tasks.models"], "models")
-GetTask = pydantic_model_creator(Task, name="Task",)
-CreateTask = pydantic_model_creator(Task, name="TaskIn", exclude_readonly=True)
 
 
 class Level(Model):
@@ -33,9 +23,23 @@ class Level(Model):
     # task = fields.ForeignKeyRelation[Task] = fields.ForeignKeyField(
     #     "models.Task", related_name="tasks")
 
+    tasks = fields.ReverseRelation["Task"]
+
+
 Tortoise.init_models(["app.tasks.models"], "models")
 GetLevel = pydantic_model_creator(Level, name="Level")
 CreateLevel = pydantic_model_creator(Level, name="LevelIn", exclude_readonly=True)
+
+
+
+class Task(Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(50)
+    level = fields.ForeignKeyField("models.Level", related_name="tasks")
+
+Tortoise.init_models(["app.tasks.models"], "models")
+GetTask = pydantic_model_creator(Task, name="Task",)
+CreateTask = pydantic_model_creator(Task, name="TaskIn", exclude_readonly=True)
 
 
 
--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -23,7 +23,7 @@ async def get_all_modules():
 #Level
 @taskRouter.post('/level', response_model=CreateLevel, name="Создание уровня")
 async def create_level(level: CreateLevel): #type: ignore
-    module = await Module.get(id=level.module_id) 
+    module = await Module.get(id=level.module_id)
     level_obj = await Level.create(title=level.title,
                                    module_id=level.module_id)
     
@@ -36,3 +36,26 @@ async def create_level(level: CreateLevel): #type: ignore
 async def all_list():
     return await GetLevel.from_queryset(Level.all())
 
+
+#Task
+@taskRouter.post('/task', response_model=CreateTask, name="Создание задачи")
+async def create_module(object_: CreateTask):  #type: ignore
+    # module = await Module.get(id=object_.module_id)
+    # level = await Level.get(id=object_.level_id)
+    task_obj = await Task.create(title=object_.title,
+                                 level_id=object_.level_id)
+    
+    return JSONResponse(content={"id":task_obj.id,
+                                 "title":task_obj.title})
+                                #  "module":module.title,
+                                #  "level":level.title})
+
+
+@taskRouter.get('/task/all', response_model=List[GetTask], description="---", name="Получение всех Задач")
+async def all_list():
+    return await GetTask.from_queryset(Task.all())
+
+
+@taskRouter.get("/task/{id}", response_model=GetTask)
+async def get_task(object_id: int):
+    return await GetTask.from_queryset_single(Task.get(id=object_id))
